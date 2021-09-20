84b29458b547df9c91b2e95a453b90940a6e5703
Hritik Kumar Singh <singhhritik978@gmail.com>
2021-09-21 00:04:22 +0530

    Added layout files

---
+++ app/src/main/java/com/example/jogtracker/db/RunDao.kt
@@ -0,0 +1,29 @@
+package com.example.jogtracker.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface RunDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRun(run: Run)
+
+    @Delete
+    suspend fun deleteRun(run: Run)
+
+    @Query("SELECT * FROM running_table ORDER BY timestamp DESC")
+    fun getAllRunsSortedByDate(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY timeInMillis DESC")
+    fun getAllRunsSortedByTimeInMills(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY caloriesBurnt DESC")
+    fun getAllRunsSortedByCaloriesBurnt(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY avgSpeed DESC")
+    fun getAllRunsSortedByAvgSpeed(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
+    fun getAllRunsSortedByDistance(): LiveData<List<Run>>
+}
